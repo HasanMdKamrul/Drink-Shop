3c0634502596a77b556b588659bf52bee8a763af
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-09-01 02:41:07 +0200

    details of section using modal added

--- js/cocktail.js
+++ js/cocktail.js
@@ -56,10 +56,15 @@ const drinkDisplay = (data,dataLimit) => {
         document.getElementById('show-all').classList.remove('d-none');
         document.getElementById('show-less').classList.add('d-none')
         drinks = drinks.slice(0,dataLimit);
-    } else{
+    } else if(drinks.length === dataLimit){
+        document.getElementById('show-all').classList.add('d-none')
+        document.getElementById('show-less').classList.add('d-none')
+    } else {
         document.getElementById('show-all').classList.add('d-none')
         document.getElementById('show-less').classList.remove('d-none')
     }
+
+
     console.log(drinks.length)
     
     drinks.forEach(drink => {
@@ -73,10 +78,11 @@ const drinkDisplay = (data,dataLimit) => {
         <div class="card-body">
             <h3 class="card-title">${strGlass ? strGlass : strDrink}</h3>
             <h2 class="card-title text-danger">${strAlcoholic ? strAlcoholic : 'N/A'}</h2>
-            <p class="card-text">${strInstructions ? strInstructions.slice(0,70) : 'No Instructions given'}</p>
-            <small class='text-info'>Drink Type:${strCategory ? strCategory : 'N/A' }</small>
+            <p class="card-text">${strInstructions ? strInstructions.slice(0,30) : 'No Instructions given'}</p>
+            <small class='text-info mb-5'>Drink Type:${strCategory ? strCategory : 'N/A' }</small>
+            <p>Date:<span class='text-primary'> ${new Date(dateModified).toLocaleDateString()}</span></p>
             <footer class='text-success'>
-                Date:<span class='text-primary'> ${new Date(dateModified).toLocaleDateString()}</span>
+                <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="loadDrinkDetails(${idDrink})" type="button" class="btn btn-outline-success">Drink Details</button>
             </footer>
         </div>
         `;
@@ -88,7 +94,50 @@ const drinkDisplay = (data,dataLimit) => {
     sppinerToggler(false)
 };
 
-// ** Search process
+
+
+// ** loadDrinkDetails
+
+const loadDrinkDetails = async id => {
+    try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+        response.ok ? console.log('Successfull') : console.log('Failed')
+        const data = await response.json();
+        displayDrinkDetail(data);
+        
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+// ** Display drink detail
+
+const displayDrinkDetail = data => {
+    // ** where to display details
+    const modalbodyContainer = document.getElementById('modal-body');
+    modalbodyContainer.textContent = ``;
+
+    const {drinks} = data;
+    
+    drinks.forEach(drink => {
+        const {strDrinkThumb,strIngredient1,strIngredient2,strIngredient3,strTags} = drink;
+
+        modalbodyContainer.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <img src="${strDrinkThumb ? strDrinkThumb : "No Image"}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${strTags ? strTags : 'N/A'}</h5>
+            <p class="card-text">${strIngredient1 ? strIngredient1 : 'N/A'}</p>
+            <p class="card-text">${strIngredient2 ? strIngredient2 : 'N/A'}</p>
+            <p class="card-text">${strIngredient3 ? strIngredient3 : 'N/A'}</p>
+        </div>
+        </div>
+        
+        `
+
+
+    })
+}
 
 
 // ** search button implimentation
@@ -115,8 +164,6 @@ const sppinerToggler = (isSpining)=>{
     isSpining ? sppiner.classList.remove('d-none') : sppiner.classList.add('d-none')
 }
 
-
-
 // ** data loaded
 loaddrinkData()
 
@@ -126,11 +173,14 @@ document.getElementById('show-all-button').addEventListener('click',()=>{
     const searchValue = inputFieldValue('search-input');
     loaddrinkData(searchValue);
 })
+
 // ** Show less Button
 document.getElementById('show-less-button').addEventListener('click',()=>{
     const searchValue = inputFieldValue('search-input');
     loaddrinkData(searchValue,4);
 })
+
+
 
 // ** 1.sppiner
 // ** 2. 20 ta kore data show kora
